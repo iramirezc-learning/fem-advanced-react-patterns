9c36d31268c8dea4d4d0075ea02211b4dcee3835
Isaac Ramirez <iramirezc@live.com.mx>
2021-03-25 01:23:17 -0600

    Introduction: - submitted my feedback

--- packages/advanced-react-patterns/src/__tests__/01.js
+++ packages/advanced-react-patterns/src/__tests__/01.js
@@ -36,8 +36,8 @@ test('can toggle back and forth', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=01&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
