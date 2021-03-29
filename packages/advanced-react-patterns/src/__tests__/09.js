c84316c6d78b9123ea54ce9ac7185dc6255b7055
Isaac Ramirez <iramirezc@live.com.mx>
2021-03-29 01:40:06 -0600

    Controlling State: - submitted feedback for exercise 09

--- packages/advanced-react-patterns/src/__tests__/09.js
+++ packages/advanced-react-patterns/src/__tests__/09.js
@@ -95,8 +95,8 @@ test('can click too much', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=09&em=iramirezc%40live.com.mx
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
