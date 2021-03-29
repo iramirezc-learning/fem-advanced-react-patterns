45776e01c57650d33ecbe088c5df74253cc5e6ff
Isaac Ramirez <iramirezc@live.com.mx>
2021-03-29 01:13:02 -0600

    Controlling State: - solved exercise 09

--- packages/advanced-react-patterns/src/exercises/09.js
+++ packages/advanced-react-patterns/src/exercises/09.js
@@ -12,8 +12,11 @@ class Toggle extends React.Component {
     onReset: () => {},
     stateReducer: (state, changes) => changes,
   }
+
   initialState = { on: this.props.initialOn }
+
   state = this.initialState
+
   internalSetState(changes, callback) {
     this.setState((state) => {
       // handle function setState call
@@ -22,34 +25,41 @@ class Toggle extends React.Component {
       // apply state reducer
       const reducedChanges =
         this.props.stateReducer(state, changesObject) || {}
+
       // 🐨  in addition to what we've done, let's pluck off the `type`
       // property and return an object only if the state changes
       // 💰 to remove the `type`, you can destructure the changes:
       // `{type, ...c}`
-      return Object.keys(reducedChanges).length
-        ? reducedChanges
-        : null
+      const { type, ...newState } = reducedChanges
+
+      return Object.keys(newState).length ? newState : null
     }, callback)
   }
+
   reset = () =>
     // 🐨 add a `type` string property to this call
-    this.internalSetState(this.initialState, () =>
-      this.props.onReset(this.state.on),
+    this.internalSetState(
+      { ...this.initialState, type: 'reset' },
+      () => this.props.onReset(this.state.on),
     )
+
   // 🐨 accept a `type` property here and give it a default value
-  toggle = () =>
+  toggle = ({ type = 'toggle' } = {}) => {
     this.internalSetState(
       // pass the `type` string to this object
-      ({ on }) => ({ on: !on }),
+      ({ on }) => ({ on: !on, type }),
       () => this.props.onToggle(this.state.on),
     )
+  }
+
   getTogglerProps = ({ onClick, ...props } = {}) => ({
     // 🐨 change `this.toggle` to `() => this.toggle()`
     // to avoid passing the click event to this.toggle.
-    onClick: callAll(onClick, this.toggle),
+    onClick: callAll(onClick, () => this.toggle()),
     'aria-pressed': this.state.on,
     ...props,
   })
+
   getStateAndHelpers() {
     return {
       on: this.state.on,
@@ -58,6 +68,7 @@ class Toggle extends React.Component {
       getTogglerProps: this.getTogglerProps,
     }
   }
+
   render() {
     return this.props.children(this.getStateAndHelpers())
   }
