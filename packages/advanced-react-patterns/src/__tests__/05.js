56fe7773575fcaa43f164a3a01248e52c9d00ddf
Isaac Ramirez <iramirezc@live.com.mx>
2021-03-28 21:18:22 -0600

    Render Props: - submitted feedback for exercise 05

--- packages/advanced-react-patterns/src/__tests__/05.js
+++ packages/advanced-react-patterns/src/__tests__/05.js
@@ -36,8 +36,8 @@ test('can also toggle with the custom button', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=05&em=iramirezc%40live.com.mx
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
