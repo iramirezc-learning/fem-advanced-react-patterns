675b1a4028cbc108b967a5318015509eae8c8982
Isaac Ramirez <iramirezc@live.com.mx>
2021-03-31 02:42:34 -0600

    Provider Pattern: - submitted feedback for exercise 11 (new version)

--- packages/advanced-react-patterns/src/__tests__/11.js
+++ packages/advanced-react-patterns/src/__tests__/11.js
@@ -51,10 +51,10 @@ test('can still use the render prop API', () => {
 // 3. Change submitted from `false` to `true`
 // 4. And you're all done!
 /*
-http://ws.kcd.im/?ws=react%20patterns&e=11&em=
+http://ws.kcd.im/?ws=react%20patterns&e=11&em=iramirezc%40live.com.mx
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
